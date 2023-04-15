db45e5149d17977ea7266a075524269e5b5fbb4f
sinsinpurin <tusanch6@gmail.com>
2023-04-15 19:47:57 +0900

    change verify qr

--- implementations/wallet/src/pages/Popup/components/polygonid-integrate/get-credential-polygonid.tsx
+++ implementations/wallet/src/pages/Popup/components/polygonid-integrate/get-credential-polygonid.tsx
@@ -3,23 +3,41 @@ import { Button } from '@mui/material';
 import { QRCode } from 'react-qr-svg';
 
 const ShowPolygonIDQR = () => {
+  const deployedContractAddress = '0x8689BFaF640410fF9bc0F79b8a864C337CA587bc';
+
   const qrProofRequestJson = {
-    id: '99d71b23-3679-43a8-8442-7fbdf6fe6248',
+    id: '7f38a193-0918-4a48-9fac-36adfdb8b542',
     typ: 'application/iden3comm-plain-json',
-    type: 'https://iden3-communication.io/credentials/1.0/offer',
-    thid: '99d71b23-3679-43a8-8442-7fbdf6fe6248',
+    type: 'https://iden3-communication.io/proofs/1.0/contract-invoke-request',
+    thid: '7f38a193-0918-4a48-9fac-36adfdb8b542',
     body: {
-      url: 'https://self-hosted-platform.polygonid.me/v1/agent',
-      credentials: [
+      reason: 'airdrop participation',
+      transaction_data: {
+        contract_address: deployedContractAddress,
+        method_id: 'b68967e2',
+        chain_id: 80001,
+        network: 'polygon-mumbai',
+      },
+      scope: [
         {
-          id: '0954e779-db63-11ed-b7df-0242c0a88005',
-          description: 'EthTokyoParticipant',
+          id: 1,
+          circuitId: 'credentialAtomicQuerySigV2OnChain',
+          query: {
+            allowedIssuers: ['*'],
+            context:
+              'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld',
+            credentialSubject: {
+              birthday: {
+                $lt: 20020101,
+              },
+            },
+            type: 'KYCAgeCredential',
+          },
         },
       ],
     },
-    from: 'did:polygonid:polygon:mumbai:2qH7XAwYQzCp9VfhpNgeLtK2iCehDDrfMWUCEg5ig5',
-    to: 'did:polygonid:polygon:mumbai:2qEtS9NPWCL1ZPS9z7BtgeRNJHc8GuHgz4XUQ6jXuG',
   };
+
   return (
     <QRCode
       level="Q"
